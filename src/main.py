09bc7d5addcf61e7d880177e3d9621092b0164c9
juangui3 <jrein008@fiu.edu>
2019-09-06 00:18:07 +0000

    link to postman

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from models import db
+from models import db, Person
 #from models import Person
 
 app = Flask(__name__)
@@ -28,14 +28,74 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/hello', methods=['POST', 'GET'])
+
+@app.route('/person', methods=['POST'])
+def post_person():
+
+    # First we get the payload json
+    body = request.get_json()
+
+    if body is None:
+        raise APIException("You need to specify the request body as a json object", status_code=400)
+
+    if 'email' not in body:
+        raise APIException('You need to specify the email', status_code=400)
+    if 'address' not in body:
+        raise APIException('You need to specify the address', status_code=400)
+
+    # at this point, all data has been validated, we can proceed to inster into the bd
+    user1 = Person(full_name=body['full_name'], email=body['email'], address=body['address'], agenda_slug=body['agenda_slug'], phone=body['phone'])
+    db.session.add(user1)
+    db.session.commit()
+    return "ok", 200
+
+@app.route('/person/<int:person_id>', methods=['PUT', 'GET'])
+def get_single_person(person_id):
+    """
+    Single person
+    """
+    if request.method == 'PUT':
+        user1 = Person.query.get(person_id)
+    body = request.get_json()
+
+    if body is None:
+        raise APIException("You need to specify the request body as a json object", status_code=400)
+    if "full_name" in body:
+        user1.full_name = body['full_name']
+    if "address" in body:
+        user1.address = body['address']
+    if "email" in body:
+        user1.email = body['email']
+    if "phone" in body:
+        user1.phone = body['phone']
+        user1.agenda_slug = "new_contact_agenda"
+        db.session.commit()
+
+        return jsonify(user1.serialize()), 200
+
+    if request.method == 'GET':
+        user1 = Person.query.get(person_id)
+        return jsonify(user1.serialize()), 200
+
+    return "Invalid Method", 404
+
+@app.route('/person/<int:person_id>', methods=['DELETE'])
+def del_single_person(person_id):
+    if request.method == 'DELETE':
+        user1 = Person.query.get(person_id)
+
+    if user1 is None:
+        raise APIException('User not found', status_code=404)
+    db.session.delete(user1)
+    db.session.commit()
+    return "ok", 200
+
+@app.route('/contacts', methods=['GET'])
 def handle_person():
 
-    response_body = {
-        "hello": "world"
-    }
-
-    return jsonify(response_body), 200
+    people_query = Person.query.all()
+    people_query =  list(map(lambda x: x.serialize(), people_query))
+    return jsonify(people_query), 200
 
 # this only runs if `$ python src/main.py` is exercuted
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,16 +2,24 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    agenda_slug = db.Column(db.String(120), unique=False, nullable=False)
+    address = db.Column(db.String(120), unique=True, nullable=False)
+    phone = db.Column(db.String(120), unique=True, nullable=False)
 
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
+    def __repr__(self):
+        return '<Person %r>' % self.full_name
 
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
+    def serialize(self):
+        return {
+            "full_name": self.full_name,
+            "email": self.email,
+            "agenda_slug": self.agenda_slug,
+            "address": self.address,
+            "phone": self.phone,
+            "id":self.id
+
+        }
